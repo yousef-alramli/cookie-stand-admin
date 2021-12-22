bdbabd619b56b4077b201a10fefe2aac5f3bcf94
yousef-alramli <ysabdys1996@gmail.com>
2021-12-22 22:12:24 +0200

    back and front-end

--- pages/components/Table.js
+++ pages/components/Table.js
@@ -30,13 +30,41 @@ function Table(props) {
                                     index % 2 == 0 ? (<tr className='bg-red-400'>
                                         <td className='px-2 border border-black' >
                                             {stand.location}
-                                            <button onClick={() => { deleteHandler(stand.id) }} className='w-8  bg-orange-50 rounded-full text-red-900 font-black float-right' >X</button>
+                                            <button
+                                                onClick={() => { deleteHandler(stand.id) }}
+                                                className='w-8 bg-orange-50 rounded-full text-red-900 font-black float-right'>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="w-8 h-8 "
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    stroke="currentColor">
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                </svg>
+                                            </button>
                                         </td>
                                         {randomized}
                                     </tr>) : (<tr className='bg-red-300'>
                                         <td className='px-2 border border-black' >
                                             {stand.location}
-                                            <button onClick={() => { deleteHandler(stand.id) }} className='w-8  bg-orange-50 rounded-full text-red-900 font-black float-right'>X</button>
+                                            <button
+                                                onClick={() => { deleteHandler(stand.id) }}
+                                                className='w-8 bg-orange-50 rounded-full text-red-900 font-black float-right'>
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    className="w-8 h-8 "
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    stroke="currentColor">
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                                </svg>
+                                            </button>
                                         </td>
                                         {randomized}
                                     </tr>)
